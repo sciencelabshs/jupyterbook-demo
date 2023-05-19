b7866b4b52cac97a235933f492d353184018948f
SHSarma <vlabshs@gmail.com>
2023-05-19 20:44:56 +0530

    added interactive coding

---
+++ _build/html/lite/extensions/@deathbeds/ipydrawio-webpack/static/480.847ef747812ad1b9ad7d.js
@@ -0,0 +1,38 @@
+"use strict";
+(self["webpackChunk_deathbeds_ipydrawio_webpack"] = self["webpackChunk_deathbeds_ipydrawio_webpack"] || []).push([[480],{
+
+/***/ 480:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// Copyright 2023 ipydrawio contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const plugin = {
+  id: '@deathbeds/ipydrawio-webpack:plugin',
+  activate: async () => {
+    //
+  },
+  autoStart: true,
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
